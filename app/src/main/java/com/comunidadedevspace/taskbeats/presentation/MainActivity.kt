343c9b5d74cb620c9cde20649f40ef084afa8e28
GeovaneJEan <geovane.99@hotmail.com>
2023-07-10 23:38:44 +0100

    new day

--- app/src/main/java/com/comunidadedevspace/taskbeats/presentation/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/taskbeats/presentation/MainActivity.kt
@@ -2,11 +2,32 @@ package com.comunidadedevspace.taskbeats.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.commit
 import com.comunidadedevspace.taskbeats.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavView = findViewById<BottomNavigationView>(R.id.bottom_nav_view)
+
+        val taskListFragment = TaskListFragment.newInstance()
+        val newsListFragment = NewsListFragment.newInstance()
+
+        bottomNavView.setOnItemSelectedListener { menuItem ->
+            val fragment = when(menuItem.itemId) {
+                R.id.task_list -> taskListFragment
+            }
+        }
+
+
+        supportFragmentManager.commit {
+            add(R.id.fragment_container_view, taskListFragment)
+            setReorderingAllowed(true)
+        }
     }
 }
---
+++ app/src/main/java/com/comunidadedevspace/taskbeats/presentation/TaskListFragment.kt
@@ -0,0 +1,40 @@
+package com.comunidadedevspace.taskbeats.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.comunidadedevspace.taskbeats.R
+
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [TaskListFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class TaskListFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_task_list, container, false)
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+
+         * @return A new instance of fragment TaskListFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance() =
+            TaskListFragment()
+    }
+}
